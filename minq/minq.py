c94e0d9b5ba1eb0727c9fa063fa39f2178b9ef34
Steve Theodore <steve@theodox.com>
2015-06-29 23:40:33 -0700

    doc

--- minq/minq.py
+++ minq/minq.py
@@ -1,4 +1,12 @@
-__author__ = 'stevet'
+"""
+This module collects all of the operators defined in the different submodules for convenience.
+
+Typical usage:
+
+    from minq.minq import *
+    scene().assemblies().where(lambda r: 'root' in r)
+
+"""
 
 from .ls import *
 from .projections import *
