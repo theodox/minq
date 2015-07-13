79870b037bc7b018f886b96ce43dcdf120159bcb
Steve Theodore <steve@theodox.com>
2015-07-12 21:54:22 -0700

    add first and last methods

--- minq/util.py
+++ minq/util.py
@@ -1,8 +1,4 @@
 import itertools
-
-__author__ = 'stevet'
-
-
 from collections import namedtuple
 XYZ = namedtuple('XYZ', 'x y z')
 
@@ -11,5 +7,25 @@ def get(expr):
     return expr.eval()
 
 
+def first(expr):
+    """
+    return only the first item in the query, or None
+    """
+    return next((i for i in expr))
+
+
+def last(expr):
+    """
+    return only the last item in the query, or None
+    """
+    final = None
+    for item in expr:
+        final = item
+    return final
+
+
 def parallel(*exprs):
+    """
+    return a zip of multiple queries. No guarantees are made about the ordering
+    """
     return itertools.izip_longest(*exprs)
